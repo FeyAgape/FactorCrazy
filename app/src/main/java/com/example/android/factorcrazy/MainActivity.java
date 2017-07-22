eb30aa172be308807c08cd88ce041847f0246889
Fey Ijaware <fshola4real@yahoo.co.uk>
2017-07-22 16:34:46 +0100

    updated grading logic

--- app/src/main/java/com/example/android/factorcrazy/MainActivity.java
+++ app/src/main/java/com/example/android/factorcrazy/MainActivity.java
@@ -13,249 +13,145 @@ import android.widget.Toast;
  * This app displays points for a quiz app.
  */
 public class MainActivity extends AppCompatActivity {
-    int points;
+    int points = 0;
+
+    // Find all views by ID
+    EditText nameEditText;
+    EditText questionOne;
+    RadioButton questionTwoAnswerA;
+    RadioButton questionThreeAnswerA;
+    RadioButton questionFourAnswerA;
+    RadioButton questionFiveAnswerA;
+    CheckBox questionSixAnswerA;
+    CheckBox questionSixAnswerB;
+    CheckBox questionSixAnswerC;
+    CheckBox questionSixAnswerD;
+    RadioButton questionSevenAnswerA;
+    RadioButton questionEightAnswerA;
+    CheckBox questionNineAnswerA;
+    CheckBox questionNineAnswerB;
+    CheckBox questionNineAnswerC;
+    RadioButton questionTenAnswerA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //find all views by id
+        nameEditText = (EditText) findViewById(R.id.name_field);
+        questionOne = (EditText) findViewById(R.id.enter_earth);
+        questionTwoAnswerA = (RadioButton) findViewById(R.id.gold_correct);
+        questionThreeAnswerA = (RadioButton) findViewById(R.id.ocean_correct);
+        questionFourAnswerA = (RadioButton) findViewById(R.id.hell_correct);
+        questionFiveAnswerA = (RadioButton) findViewById(R.id.mountain_correct);
+        questionSixAnswerA = (CheckBox) findViewById(R.id.answerA_checkbox);
+        questionSixAnswerB = (CheckBox) findViewById(R.id.answerB_checkbox);
+        questionSixAnswerC = (CheckBox) findViewById(R.id.answerC_checkbox);
+        questionSixAnswerD = (CheckBox) findViewById(R.id.answerD_checkbox);
+        questionSevenAnswerA = (RadioButton) findViewById(R.id.volcano_correct);
+        questionEightAnswerA = (RadioButton) findViewById(R.id.pacific_correct);
+        questionNineAnswerA = (CheckBox) findViewById(R.id.saharaA_checkbox);
+        questionNineAnswerB = (CheckBox) findViewById(R.id.saharaB_checkbox);
+        questionNineAnswerC = (CheckBox) findViewById(R.id.saharaC_checkbox);
+        questionTenAnswerA = (RadioButton) findViewById(R.id.reef_correct);
     }
 
     /**
      * This method is called when question 1 is answered.
      */
-    public void submitAnswer(View view) {
-        EditText enterAnswer = (EditText) findViewById(R.id.enter_earth);
-        String yourAnswer = enterAnswer.getText().toString();
-        if (yourAnswer.equals(getString(R.string.answer1)))
-            points = points + 2;
+
+    public void questionOneSubmit(View view) {
+
+        if (questionOne.getText().toString().equals(getString(R.string.answer1))) {
+            points += 2;
+        }
     }
 
     /**
      * This method is called when question 2 is answered.
      */
-    public void goldRadio(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch (view.getId()) {
-            case R.id.gold_correct:
-                if (checked)
-                    //allows the answers button to only be checked once
-                    findViewById(R.id.gold_correct).setClickable(false);
-                points = points + 2;
-                break;
-            case R.id.gold_wrong:
-                if (checked)
-                    findViewById(R.id.gold_correct).setClickable(false);
-                points = 0;
-                break;
+    public void questionTwoSubmit(View view) {
+        if (questionTwoAnswerA.isChecked()) {
+            points += 2;
         }
     }
 
     /**
      * This method is called when question 3 is answered.
      */
-    public void oceanRadio(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch (view.getId()) {
-            case R.id.ocean_correct:
-                if (checked)
-                    findViewById(R.id.ocean_correct).setClickable(false);
-                points = points + 2;
-                break;
-            case R.id.ocean_wrong:
-                if (checked)
-                    findViewById(R.id.ocean_wrong).setClickable(false);
-                points = 0;
-                break;
+    public void questionThreeSubmit(View view) {
+        if (questionThreeAnswerA.isChecked()) {
+            points += 2;
         }
     }
 
     /**
      * This method is called when question 4 is answered.
      */
-    public void hellRadio(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch (view.getId()) {
-            case R.id.hell_correct:
-                if (checked)
-                    findViewById(R.id.hell_correct).setClickable(false);
-                points = points + 2;
-                break;
-            case R.id.hell_wrong:
-                if (checked)
-                    findViewById(R.id.hell_wrong).setClickable(false);
-                points = 0;
-                break;
+    public void questionFourSubmit(View view) {
+        if (questionFourAnswerA.isChecked()) {
+            points += 2;
         }
     }
+
 
     /**
      * This method is called when question 5 is answered.
      */
-    public void mountainRadio(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch (view.getId()) {
-            case R.id.mountain_correct:
-                if (checked)
-                    findViewById(R.id.mountain_correct).setClickable(false);
-                points = points + 2;
-                break;
-            case R.id.mountain_wrong:
-                if (checked)
-                    findViewById(R.id.mountain_wrong).setClickable(false);
-                points = 0;
-
-                break;
+    public void questionFiveSubmit(View view) {
+        if (questionFiveAnswerA.isChecked()) {
+            points += 2;
         }
     }
 
     /**
      * This method is called when question 6 is answered.
      */
-    public void onCheckboxClicked(View view) {
-        // Is the view now checked?
-        boolean checked = ((CheckBox) view).isChecked();
-        // Show a message as a toast
-        Toast.makeText(this, getString(R.string.toast2), Toast.LENGTH_SHORT).show();
-        // Exit this method early because there's nothing left to do
+    public void questionSixSubmit(View view) {
 
-        // Check which checkbox was clicked
-        switch (view.getId()) {
-            case R.id.answerA_checkbox:
-                if (checked)
-
-                    findViewById(R.id.answerA_checkbox).setClickable(false);
-                // Add two points
-                points = points + 2;
-                break;
-
-            case R.id.answerB_checkbox:
-                if (checked)
-                    findViewById(R.id.answerB_checkbox).setClickable(false);
-                points = points + 2;
-                break;
-
-            case R.id.answerC_checkbox:
-                if (checked)
-                    findViewById(R.id.answerC_checkbox).setClickable(false);
-                points = 0;
-                break;
-
-            case R.id.answerD_checkbox:
-                if (checked)
-                    findViewById(R.id.answerD_checkbox).setClickable(false);
-                points = 0;
-                break;
+        if ((questionSixAnswerA.isChecked()) && (questionSixAnswerB.isChecked()) && (!questionSixAnswerC.isChecked())
+                && (!questionSixAnswerD.isChecked())) {
+            points += 4;
         }
     }
 
     /**
      * This method is called when question 7 is answered.
      */
-    public void volcanoRadio(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch (view.getId()) {
-            case R.id.volcano_correct:
-                if (checked)
-                    findViewById(R.id.volcano_correct).setClickable(false);
-                points = points + 2;
-                break;
-            case R.id.volcano_wrong:
-                if (checked)
-                    findViewById(R.id.volcano_wrong).setClickable(false);
-                points = 0;
-                break;
+    public void questionSevenSubmit(View view) {
+        if (questionSevenAnswerA.isChecked()) {
+            points += 2;
         }
     }
 
     /**
      * This method is called when question 8 is answered.
      */
-    public void pacificRadio(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch (view.getId()) {
-            case R.id.pacific_correct:
-                if (checked)
-                    findViewById(R.id.pacific_correct).setClickable(false);
-                points = points + 2;
-                break;
-            case R.id.pacific_wrong:
-                if (checked)
-                    findViewById(R.id.pacific_wrong).setClickable(false);
-                points = 0;
-                break;
+    public void questionEightSubmit(View view) {
+        if (questionEightAnswerA.isChecked()) {
+            points += 2;
         }
-
     }
 
     /**
      * This method is called when question 9 is answered.
      */
-    public void saharaCheckboxClicked(View view) {
-        // Is the view now checked?
-        boolean checked = ((CheckBox) view).isChecked();
-        // Show a message as a toast
-        Toast.makeText(this, getString(R.string.toast2), Toast.LENGTH_SHORT).show();
-        // Exit this method early because there's nothing left to do
+    public void questionNineSubmit(View view) {
 
-        // Check which checkbox was clicked
-        switch (view.getId()) {
-            case R.id.saharaA_checkbox:
-                if (checked)
-                    points = points + 2;
-                // Add two points
-
-                break;
-            case R.id.saharaB_checkbox:
-                if (checked)
-                    points = points + 2;
-                break;
-
-            case R.id.saharaC_checkbox:
-                if (checked)
-                    findViewById(R.id.answerD_checkbox).setClickable(false);
-                points = 0;
-                break;
+        if ((questionNineAnswerA.isChecked()) && (questionNineAnswerB.isChecked())
+                && (!questionNineAnswerC.isChecked())) {
+            points += 4;
         }
     }
 
     /**
      * This method is called when question 10 is answered.
      */
-    public void reefRadio(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch (view.getId()) {
-            case R.id.reef_correct:
-                if (checked)
-                    findViewById(R.id.reef_correct).setClickable(false);
-                points = points + 2;
-                break;
-            case R.id.reef_wrong:
-                if (checked)
-                    findViewById(R.id.reef_wrong).setClickable(false);
-                points = 0;
-                break;
+    public void questionTenSubmit(View view) {
+        if (questionTenAnswerA.isChecked()) {
+            points += 2;
         }
     }
 
@@ -266,6 +162,7 @@ public class MainActivity extends AppCompatActivity {
         //Name field
         EditText nameField = (EditText) findViewById(R.id.name_field);
         String name = nameField.getText().toString();
+
 
         String submitMessage = name + getString(R.string.toast3a) + " " + points + " " + getString(R.string.toast3c);
         String submitMessageTwo = name + getString(R.string.toast3b) + " " + points + " " + getString(R.string.toast3c);
@@ -278,6 +175,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, submitMessageTwo, Toast.LENGTH_LONG).show();
         }
     }
+
 
     /**
      * This method is called when the reset button is clicked.
